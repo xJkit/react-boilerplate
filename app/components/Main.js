069e6239080f4f57db64658b7c936d6005fc142b
xJkit-iMac <xjkit54780@gmail.com>
2016-10-12 02:16:50 +0800

    Add Main component

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  rener(){
+    return(
+      <div>Main component rendered successfully.</div>
+    )
+  }
+}
+
+export default Main
