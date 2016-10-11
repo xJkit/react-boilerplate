12d6332e7188088c4b6c984a9df20e2a9d328920
xJkit-iMac <xjkit54780@gmail.com>
2016-10-12 02:14:04 +0800

    Modify app.js into ES6

--- app/app.js
+++ app/app.js
@@ -1,14 +1,17 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Route, Router, IndexRoute, hashHistory} from 'react-router'
 
 // Load foundation
 $(document).foundation();
 
-// App css
-require('style!css!sass!applicationStyles')
+// Load custom css: app.scss
+import 'style!css!sass!applicationStyles'
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
-  document.getElementById('app')
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>
+  ,document.getElementById('app')
 );
