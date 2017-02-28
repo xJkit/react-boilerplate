71716e0247370dd6d8872baa91c2a4cc2ea3105b
xJkit-iMac <xjkit54780@gmail.com>
2017-03-01 00:35:18 +0800

    Fix package.json additional comma and useless reduces

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import step from './step';
+
+/* import custom reducers below */
 
 const rootReducers = combineReducers({
   routing: routerReducer,
-  step,
 });
 
 export default rootReducers;
